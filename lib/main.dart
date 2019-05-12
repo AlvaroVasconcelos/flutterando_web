7cb2506fd780949cbb089e3ab8046a1a25e31a9a
rullyalves <rullyalvesz@gmail.com>
2019-05-12 03:06:22 -0300

    Adicionando novas páginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_web/material.dart';
 import 'package:flutter_web/widgets.dart';
-
-import 'shared/widget/Gradient_AppBar.dart';
-import 'shared/widget/Link_Button.dart';
+import 'package:flutterando_web/home/HomePage.dart';
 
 void main() => runApp(App());
 
@@ -15,48 +13,6 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: GradientAppBar(
-        image: 'images/logo.png',
-        buttons: [
-          LinkButton(
-            label: 'Home',
-            onTap: () {},
-          ),
-          LinkButton(
-            label: 'Sobre',
-            onTap: () {},
-          ),
-          LinkButton(
-            label: 'Equipe',
-            onTap: () {},
-          ),
-          LinkButton(
-            label: 'Contato',
-            onTap: () {},
-          ),
-          Spacer()
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Hello, World!'),
-          ],
-        ),
-      ),
     );
   }
 }
