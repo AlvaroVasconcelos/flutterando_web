e65fd68eeb551d9b02fb256f62c56c81784b248c
jacobmoura7 <tecleigo@gmail.com>
2019-05-19 03:11:48 -0300

    add contact

--- lib/screens/contact/contact_page.dart
+++ lib/screens/contact/contact_page.dart
@@ -1,4 +1,8 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter_web/material.dart';
+import 'components/bottom_bar.dart';
+import 'components/contact_container.dart';
+import 'components/contact_form.dart';
 
 class ContactPage extends StatefulWidget {
   ContactPage({Key key}) : super(key: key);
@@ -6,13 +10,44 @@ class ContactPage extends StatefulWidget {
   _ContactPageState createState() => _ContactPageState();
 }
 
-
 class _ContactPageState extends State<ContactPage> {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Container(
-      color: Colors.pink,
+      padding: const EdgeInsets.all(15),
+      margin: EdgeInsets.only(top: size.width * 0.075),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: Row(
+              children: <Widget>[
+                Spacer(flex: 1),
+                Expanded(
+                  flex: 3,
+                  child: AnimatedCard(
+                    direction: AnimatedCardDirection.left,
+                    initDelay: Duration(seconds: 0),
+                    child: ContactContainer(),
+                  ),
+                ),
+                Spacer(flex: 1),
+                Expanded(
+                  flex: 6,
+                  child: AnimatedCard(
+                    direction: AnimatedCardDirection.right,
+                    initDelay: Duration(seconds: 0),
+                    child: ContactForm(),
+                  ),
+                ),
+                Spacer(flex: 1),
+              ],
+            ),
+          ),
+          Expanded(flex: 2, child: BottomBar())
+        ],
+      ),
     );
   }
 }
-
